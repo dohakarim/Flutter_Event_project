e4a0bd77294ec6bf73b29ce51e0a46416153533c
dohakarim <148819314+dohakarim@users.noreply.github.com>
2024-04-13 22:21:03 +0000

    Update sql_helper.dart

--- lib/sql_helper.dart
+++ lib/sql_helper.dart
@@ -3,21 +3,26 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 class SQLHelper {
   static Future<void> createTables(sql.Database database) async {
-    await database.execute("""CREATE TABLE items(
-        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        title TEXT,
-        description TEXT,
-        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      )
-      """);
-  }
-// id: the id of a item
-// title, description: name and description of your activity
+  await database.execute("""CREATE TABLE evenement(
+      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+      titre TEXT,
+      description TEXT,
+      lieu TEXT,
+      type TEXT,
+      date_evenement TEXT,
+      image TEXT, 
+      createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    )
+    """);
+  // print('Table "evenement" created successfully.');
+}
+
+
 // created_at: the time that the item was created. It will be automatically handled by SQLite
 
   static Future<sql.Database> db() async {
     return sql.openDatabase(
-      'dbtech.db',
+      'dbevents.db',
       version: 1,
       onCreate: (sql.Database database, int version) async {
         await createTables(database);
@@ -26,49 +31,59 @@ class SQLHelper {
   }
 
   // Create new item (journal)
-  static Future<int> createItem(String title, String? descrption) async {
-    final db = await SQLHelper.db();
+  static Future<int> createItem(String titre, String? description, String lieu, String type, String dateEvenement, String image) async {
+  final db = await SQLHelper.db();
 
-    final data = {'title': title, 'description': descrption};
-    final id = await db.insert('items', data,
-        conflictAlgorithm: sql.ConflictAlgorithm.replace);
-    return id;
-  }
+  final data = {
+    'titre': titre,
+    'description': description,
+    'lieu': lieu,
+    'type': type,
+    'date_evenement': dateEvenement,
+    'image': image 
+  };
+
+  final id = await db.insert('evenement', data, conflictAlgorithm: sql.ConflictAlgorithm.replace);
+  return id;
+}
+
 
   // Read all items (journals)
   static Future<List<Map<String, dynamic>>> getItems() async {
     final db = await SQLHelper.db();
-    return db.query('items', orderBy: "id");
+    return db.query('evenement', orderBy: "id");
   }
 
   // Read a single item by id
   // The app doesn't use this method but I put here in case you want to see it
   static Future<List<Map<String, dynamic>>> getItem(int id) async {
     final db = await SQLHelper.db();
-    return db.query('items', where: "id = ?", whereArgs: [id], limit: 1);
+    return db.query('evenement', where: "id = ?", whereArgs: [id], limit: 1);
   }
 
   // Update an item by id
-  static Future<int> updateItem(
-      int id, String title, String? descrption) async {
-    final db = await SQLHelper.db();
+  static Future<int> updateItem(int id, String titre, String? description, String lieu, String type, String image, String dateEvenement) async {
+  final db = await SQLHelper.db();
 
-    final data = {
-      'title': title,
-      'description': descrption,
-      'createdAt': DateTime.now().toString()
-    };
+  final data = {
+    'titre': titre,
+    'description': description,
+    'lieu': lieu,
+    'type': type,
+    'image': image, // Ajout du champ image
+    'date_evenement': dateEvenement.toString(), // Conversion de la date en String
+    'createdAt': DateTime.now().toString()
+  };
 
-    final result =
-    await db.update('items', data, where: "id = ?", whereArgs: [id]);
-    return result;
-  }
+  final result = await db.update('evenement', data, where: "id = ?", whereArgs: [id]);
+  return result;
+}
 
   // Delete
   static Future<void> deleteItem(int id) async {
     final db = await SQLHelper.db();
     try {
-      await db.delete("items", where: "id = ?", whereArgs: [id]);
+      await db.delete("evenement", where: "id = ?", whereArgs: [id]);
     } catch (err) {
       debugPrint("Something went wrong when deleting an item: $err");
     }
