d5feb4ae2ece01e2b7c1bb19192e03def2e6cec3
dohakarim <148819314+dohakarim@users.noreply.github.com>
2024-04-13 22:20:13 +0000

    Update setting_page.dart

--- lib/page/setting_page.dart
+++ lib/page/setting_page.dart
@@ -56,12 +56,25 @@ class _CreatEventForm extends State<CreatEvent> {
   bool _isLoading = true;
   // This function is used to fetch all data from the database
   void _refreshJournals() async {
+  setState(() {
+    _isLoading = true; // Mettre isLoading à true au début du chargement
+  });
+
+  try {
     final data = await SQLHelper.getItems();
     setState(() {
       _journals = data;
-      _isLoading = false;
+      _isLoading = false; // Mettre isLoading à false une fois que les données sont récupérées
+    });
+  } catch (error) {
+    // Gérer les erreurs éventuelles lors de la récupération des données
+    debugPrint("Error fetching journals: $error");
+    setState(() {
+      _isLoading = false; // Mettre isLoading à false en cas d'erreur
     });
   }
+}
+
 
   @override
   void initState() {
@@ -71,17 +84,24 @@ class _CreatEventForm extends State<CreatEvent> {
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _lieuController = TextEditingController();
+  final TextEditingController _genreController = TextEditingController();
+  final TextEditingController _dateEventController = TextEditingController();
+  final TextEditingController _imageController = TextEditingController();
+
 
   // This function will be triggered when the floating button is pressed
   // It will also be triggered when you want to update an item
   void _showForm(int? id) async {
-    if (id != null) {
-      // id == null -> create new item
-      // id != null -> update an existing item
-      final existingJournal =
-      _journals.firstWhere((element) => element['id'] == id);
-      _titleController.text = existingJournal['title'];
-      _descriptionController.text = existingJournal['description'];
+   if (id != null) {
+    final existingJournal =
+    _journals.firstWhere((element) => element['id'] == id);
+    _titleController.text = existingJournal['titre'];
+    _descriptionController.text = existingJournal['description'];
+    _lieuController.text = existingJournal['lieu'];
+    _genreController.text = existingJournal['type'];
+    _dateEventController.text = existingJournal['date_evenement'];
+    _imageController.text = existingJournal['image'];
     }
 
     showModalBottomSheet(
@@ -101,20 +121,46 @@ class _CreatEventForm extends State<CreatEvent> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
-              TextField(
-                controller: _titleController,
-                decoration: const InputDecoration(hintText: 'Title'),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
+                TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(hintText: 'Titre'),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
               TextField(
                 controller: _descriptionController,
                 decoration: const InputDecoration(hintText: 'Description'),
               ),
               const SizedBox(
-                height: 20,
+                height: 10,
               ),
+              TextField(
+                controller: _lieuController,
+                decoration: const InputDecoration(hintText: 'Lieu'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _genreController,
+                decoration: const InputDecoration(hintText: 'Genre'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _dateEventController,
+                decoration: const InputDecoration(hintText: 'Date de l\'événement'),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextField(
+                controller: _imageController,
+                decoration: const InputDecoration(hintText: 'Image'),
+              ),
+
               ElevatedButton(
                 onPressed: () async {
                   // Save new journal
@@ -127,14 +173,22 @@ class _CreatEventForm extends State<CreatEvent> {
                   }
 
                   // Clear the text fields
-                  _titleController.text = '';
-                  _descriptionController.text = '';
+                  _titleController.clear();
+                  _descriptionController.clear();
+                  _lieuController.clear();
+                  _genreController.clear();
+                  _dateEventController.clear();
+                  _imageController.clear();
 
                   // Close the bottom sheet
                   Navigator.of(context).pop();
+
+                  // Refresh the journal list after adding or updating an event
+                  _refreshJournals();
                 },
                 child: Text(id == null ? 'Create New' : 'Update'),
               )
+
             ],
            ),
           ),
@@ -142,18 +196,66 @@ class _CreatEventForm extends State<CreatEvent> {
   }
 
 // Insert a new journal to the database
-  Future<void> _addItem() async {
-    await SQLHelper.createItem(
-        _titleController.text, _descriptionController.text);
-    _refreshJournals();
+  // Insertion d'un nouvel événement dans la base de données
+// Insert a new journal to the database
+// Insérer un nouvel événement dans la base de données
+Future<void> _addItem() async {
+  // Vérifier si les champs requis sont vides
+  if (_titleController.text.isEmpty ) {
+    // Afficher un message d'erreur ou une alerte à l'utilisateur
+    // Ici, je vais simplement imprimer un message dans la console
+    print('Veuillez remplir tous les champs obligatoires.');
+    return; // Sortir de la méthode sans ajouter l'événement
   }
 
-  // Update an existing journal
-  Future<void> _updateItem(int id) async {
-    await SQLHelper.updateItem(
-        id, _titleController.text, _descriptionController.text);
-    _refreshJournals();
-  }
+  // Ajoutez l'événement à la base de données
+  await SQLHelper.createItem(
+    _titleController.text,
+    _descriptionController.text,
+    _lieuController.text,
+    _genreController.text,
+    _dateEventController.text,
+    _imageController.text
+  );
+
+  // Effacer les valeurs des contrôleurs après l'ajout d'un nouvel événement
+  // _titleController.clear();
+  // _descriptionController.clear();
+  // _lieuController.clear();
+  // _genreController.clear();
+  // _dateEventController.clear();
+  // _imageController.clear();
+
+  // Rafraîchir la liste des événements après l'ajout d'un nouvel événement
+  _refreshJournals();
+}
+
+
+
+// Update an existing item
+// Mettre à jour un événement existant
+Future<void> _updateItem(int id) async {
+  await SQLHelper.updateItem(
+    id,
+    _titleController.text,
+    _descriptionController.text,
+    _lieuController.text,
+    _genreController.text,
+    _dateEventController.text,
+    _imageController.text
+  );
+  // Effacez les valeurs des contrôleurs après la mise à jour de l'événement
+  // _titleController.clear();
+  // _descriptionController.clear();
+  // _lieuController.clear();
+  // _genreController.clear();
+  // _dateEventController.clear();
+  // _imageController.clear();
+  // Rafraîchissez la liste des événements après la mise à jour de l'événement
+  _refreshJournals();
+}
+
+
 
   // Delete an item
   void _deleteItem(int id) async {
@@ -168,7 +270,7 @@ class _CreatEventForm extends State<CreatEvent> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('SQL'),
+        title: const Text('les événements '),
       ),
       body: _isLoading
           ? const Center(
@@ -180,8 +282,41 @@ class _CreatEventForm extends State<CreatEvent> {
           color: Colors.orange[200],
           margin: const EdgeInsets.all(15),
           child: ListTile(
-              title: Text(_journals[index]['title']),
-              subtitle: Text(_journals[index]['description']),
+              title: Text(_journals[index]['titre']),
+             subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(Icons.description),
+              SizedBox(width: 5),
+              Text(_journals[index]['description']),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.location_on),
+              SizedBox(width: 5),
+              Text(_journals[index]['lieu']),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.event_note),
+              SizedBox(width: 5),
+              Text(_journals[index]['type']),
+            ],
+          ),
+          Row(
+            children: [
+              Icon(Icons.event),
+              SizedBox(width: 5),
+              Text(_journals[index]['date_evenement']),
+            ],
+          ),
+          // Ajoutez d'autres champs ici selon vos besoins
+        ],
+      ),
               trailing: SizedBox(
                 width: 100,
                 child: Row(
