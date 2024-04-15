238ffc2fdade451fd4e51e1f70fcb74203ab8e4c
dohakarim <148819314+dohakarim@users.noreply.github.com>
2024-04-15 03:34:46 +0100

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_project1/page/calendar.dart';
-import 'package:flutter_project1/page/chat_page.dart';
-import 'package:flutter_project1/page/dashboard_page.dart';
-import 'package:flutter_project1/page/setting_page.dart';
+import 'package:flutter_project1/page/CreatEvent_page.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
@@ -14,15 +12,12 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int currentTab = 0;
   final List<Widget> screens = [
-    const DashBoard(),
-    const ChatPage(),
-    const CreatEvent()
+    const CreatEvent(),
   ];
 
   final PageStorageBucket bucket = PageStorageBucket();
-  Widget currentScreen = DashBoard();
-  // Créez une instance de CreatEvent
-  // final CreatEvent creatEventPage = CreatEvent();
+  Widget currentScreen = CreatEvent();
+
 
   @override
   Widget build(BuildContext context) {
@@ -34,21 +29,34 @@ class _HomeState extends State<Home> {
       floatingActionButton: FloatingActionButton(
         
         onPressed: () {
-          // Appelez la fonction _showForm de CreatEvent
-              // creatEventPage.showForm(null); 
-        },
-        child: Icon(
-          Icons.add,
-          color:  Color.fromARGB(255, 26, 125, 206),
-        ),
+        setState(() {
+          currentScreen = CreatEvent();
+          currentTab = 4;
+        });
+      },
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+         children: [
+          Icon(
+            Icons.school_outlined,
+            color: Color.fromARGB(255, 26, 125, 206),
+          ),
+          Text(
+            'FSTT',
+            style: TextStyle(
+              color: currentTab == 4 ? Colors.greenAccent : Colors.blue,
+            ),
+          ),
+        ],
       ),
+    ),
       
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
         shape: CircularNotchedRectangle(),
         notchMargin: 10,
         child: Container(
-          height: 60,
+          height: 5,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -56,84 +64,7 @@ class _HomeState extends State<Home> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = DashBoard();
-                        currentTab = 0;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.dashboard,
-                          color: currentTab == 0 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Dashboard',
-                          style: TextStyle(
-                            color: currentTab == 0 ? Colors.blue : Colors.grey,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = ChatPage();
-                        currentTab = 1;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.chat,
-                          color: currentTab == 1 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Chat',
-                          style: TextStyle(
-                            color: currentTab == 1 ? Colors.blue : Colors.grey,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  MaterialButton(
-                    minWidth: 40,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = Calendar();
-                        currentTab = 3;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.person,
-                          color: currentTab == 3 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Profile',
-                          style: TextStyle(
-                            color: currentTab == 3 ? Colors.blue : Colors.grey,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  MaterialButton(
-                    minWidth: 40,
+                    minWidth: 100,
                     onPressed: () {
                       setState(() {
                         currentScreen = CreatEvent();
@@ -144,18 +75,72 @@ class _HomeState extends State<Home> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Icon(
-                          Icons.settings,
-                          color: currentTab == 4 ? Colors.blue : Colors.grey,
+                          Icons.format_list_bulleted_add,
+                          color: currentTab == 4 ? Colors.greenAccent : Colors.blue,
                         ),
                         Text(
-                          'Setting',
+                          'Events',
                           style: TextStyle(
-                            color: currentTab == 4 ? Colors.blue : Colors.grey,
+                            color: currentTab == 4 ? Colors.greenAccent : Colors.blue,
                           ),
                         )
                       ],
                     ),
                   ),
+                  // MaterialButton(
+                  //   minWidth: 40,
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       currentScreen = ChatPage();
+                  //       currentTab = 1;
+                  //     });
+                  //   },
+                  //   child: Column(
+                  //     mainAxisAlignment: MainAxisAlignment.center,
+                  //     children: [
+                  //       Icon(
+                  //         Icons.chat,
+                  //         color: currentTab == 1 ? Colors.blue : Colors.grey,
+                  //       ),
+                  //       Text(
+                  //         'Chat',
+                  //         style: TextStyle(
+                  //           color: currentTab == 1 ? Colors.blue : Colors.grey,
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                  // ),
+                ],
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    minWidth: 100,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Calendar();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.calendar_month,
+                          color: currentTab == 3 ? Colors.greenAccent : Colors.blue,
+                        ),
+                        Text(
+                          'calendrier',
+                          style: TextStyle(
+                            color: currentTab == 3 ? Colors.greenAccent : Colors.blue,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  
                 ],
               )
             ],
